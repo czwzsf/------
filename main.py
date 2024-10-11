4474633e9db0ad8a457cc8d9773e6d57b84d5ab5
chenzhewei <1241770441@qq.com>
2024-10-11 13:41:39 +0800

    分离标题V2（车型、平台、底盘号与方案号分割成功）

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ import re
 # 文件地址
 path = 'data/Data_JH6'
 
+# 车型平台库
+car_models = ["JH6", "悍V", "JH5", "鹰途", "龙V"]
+
 # 初始化一个空的列表来存储文件信息
 data = []
 
@@ -17,16 +20,21 @@ for filename in os.listdir(path):
         match = re.search(r'-(\w+)-(\w+)\((\w+)\)', filename)
         if match:
             # 获取车型平台与品系
-            car_model = match.group(1)
+            car_model_platform = match.group(1)
             # 获取车辆底盘号
             chassis_number = match.group(2)
             # 获取方案号
             scheme_number = match.group(3)
+
+            # 分离车型和平台
+            car_model = next((model for model in car_models if car_model_platform.startswith(model)), None)
+            platform = car_model_platform[len(car_model):] if car_model else car_model_platform
+
             # 将信息添加到列表中
-            data.append([car_model, chassis_number, scheme_number])
+            data.append([car_model, platform, chassis_number, scheme_number])
 
 # 将列表转换为DataFrame
-df = pd.DataFrame(data, columns=['车型平台与品系', '车辆底盘号', '方案号'])
+df = pd.DataFrame(data, columns=['车型', '平台', '车辆底盘号', '方案号'])
 
 # 打印DataFrame
 print(df)
